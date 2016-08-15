7008456bdeea1394140d27443a00c3d6e3349295
Anita <a.nita@hotmail.co.uk>
2016-08-15 15:34:29 +0100

    added AddTodo component with tests

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -7,12 +8,16 @@ var TodoApp = React.createClass({
       todos: [{id: 1, text: 'Walk the dog'}, {id: 2, text: 'Feed the cat'},  {id: 3, text: 'Clean the car'},  {id: 4, text: 'Wash dishes'}]
     }
   },
+  handleAddTodo: function(text) {
+    alert('text ' + text)
+  },
   render: function () {
     var {todos} = this.state;
 
     return (
       <div>
         <TodoList todos={todos} />
+        <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
     );
   }
---
+++ tests/components/AddTodo.test.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var AddTodo = require('AddTodo');
+
+describe('AddTodo', () => {
+  it('should exist', () => {
+    expect(AddTodo).toExist();
+  });
+
+  it('should call onAddTodo prop with valid data', () => {
+    var spy = expect.createSpy();
+    var addtodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy} />);
+    var $el = $(ReactDOM.findDOMNode(addtodo));
+
+    addtodo.refs.todoText.value = 'Check mail';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith('Check mail');
+  });
+
+  it('should not call onAddTodo prop with invalid data', () => {
+    var spy = expect.createSpy();
+    var addtodo = TestUtils.renderIntoDocument(<AddTodo onAddTodo={spy} />);
+    var $el = $(ReactDOM.findDOMNode(addtodo));
+
+    addtodo.refs.todoText.value = '';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
+  });
+});
