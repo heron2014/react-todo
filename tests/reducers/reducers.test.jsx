90bcdf72d085e9277ce917751fbd25497c96364a
Anita <a.nita@hotmail.co.uk>
2016-08-17 16:03:15 +0100

    add showCompleted and searchText reducers see https://github.com/heron2014/react-todo/issues/1

---
+++ tests/reducers/reducers.test.jsx
@@ -0,0 +1,33 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('should set searchText', () => {
+      // action that we want to dispatch
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'dog'
+      };
+
+      // we pass default state and our custom action
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompletedReducer', () => {
+    it('should toggle showCompleted', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+
+      var res = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(res).toEqual(true);
+    });
+  });
+});
