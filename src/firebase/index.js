1640386dfc2031add70c7ec19d8a4170595fae29
Anita <a.nita@hotmail.co.uk>
2016-09-15 11:16:10 +0100

    added env files for testing and developement

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
-require('env2')('.env');
+
 // Initialize Firebase
 try {
   var config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
